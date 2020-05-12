0df3ff623183d34919743cad430be2f7ec8e3ab2
AldoIrvine111 <aldoirvine@gmail.com>
2020-05-12 18:58:39 +1000

    Admin role implemented, only admin can create guide and make others as admin

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,12 @@ admin.initializeApp();
 
 //onCall to call functions from FrontEnd
 exports.addAdminRole = functions.https.onCall((data, context) => {
+
+    //Check request is made by an ADMIN
+    if(context.auth.token.admin !== true){
+        return{error: 'Only admin can add other admin. Nice Try'}
+    }
+    
     //Get user and add custom calim (admin)
     return admin.auth().getUserByEmail(data.email)
         .then(user => {
@@ -20,3 +26,13 @@ exports.addAdminRole = functions.https.onCall((data, context) => {
             return err;
         });
 });
+
+
+/*------------------------------------------------------------
+1. npm install firebase-tools -g 
+2. firebase login
+3. firebase init functions
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+
+Steps to deploy
+firebase deploy --only functions*/
